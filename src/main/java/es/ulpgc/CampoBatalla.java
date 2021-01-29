ea893a936ba342fbf3fc55d890beac95b0795cf1
AlexisAK <alexisoalvarez94@gmail.com>
2021-01-29 13:10:33 +0000

    should_saved_if_two_bombs_not_explode_around and should_return_empty_if_two_bombs_explode_around

--- src/main/java/es/ulpgc/CampoBatalla.java
+++ src/main/java/es/ulpgc/CampoBatalla.java
@@ -1,14 +1,34 @@
-package test;
+package es.ulpgc;
 
 public class CampoBatalla {
     public static String survivor(String campo) {
-        if (campo.indexOf('#') == -1) return campo.replaceAll("[\\[\\]\\s]", "");
+        if (numberOfBombsOrRefugees(campo, "#") == 0) return campo.replaceAll("[\\[\\]\\s]", "");
+        if (numberOfBombsOrRefugees(campo, "[") == 1 && numberOfBombsOrRefugees(campo, "#") == 2) return "";
+        if(numberOfBombsOrRefugees(campo, "[") >= 2 && numberOfBombsOrRefugees(campo, "#") >= 2) return verifyMoreRefugees(campo);
         return getRefugeesSaved(campo);
     }
 
-    private static String getRefugeesSaved(String campo) {
-        int corcheteInit = campo.indexOf("[");
-        int corcheteFin = campo.indexOf("]");
-        return campo.substring(corcheteInit + 1, corcheteFin);
+    private static String getRefugeesSaved(String camp) {
+        int corcheteInit = camp.indexOf("[");
+        int corcheteFin = camp.indexOf("]");
+        return camp.substring(corcheteInit + 1, corcheteFin);
     }
+
+    public static int numberOfBombsOrRefugees(String camp, String character) {
+        int posicion, cont = 0;
+        posicion = camp.indexOf(character);
+        while (posicion != -1) {
+            cont++;
+            posicion = camp.indexOf(character, posicion + 1);
+        }
+        return cont;
+    }
+
+    public static String verifyMoreRefugees(String campo) {
+        int corcheteFin = campo.indexOf("]");
+        String newCamp = campo.substring(corcheteFin+1).trim();
+        if(numberOfBombsOrRefugees(newCamp, "#") == 0 || numberOfBombsOrRefugees(newCamp, "#") == 1) return getRefugeesSaved(newCamp);
+        return "";
+    }
+
 }
--- src/test/java/test/CampoBatalla_.java
+++ src/test/java/test/CampoBatalla_.java
@@ -1,2 +1,36 @@
-package test;public class CampoBatalla_ {
+package test;
+
+import es.ulpgc.CampoBatalla;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CampoBatalla_ {
+
+    @Test
+    public void should_return_all_battlecamp_if_not_explode_bomb_around(){
+        assertThat(CampoBatalla.survivor("abde [fgh] ijk")).isEqualTo("abdefghijk");
+        assertThat(CampoBatalla.survivor("abde [aaa] ijk")).isEqualTo("abdeaaaijk");
+    }
+
+    @Test
+    public void should_refugees_saved_when_one_bomb_explode_around(){
+        assertThat(CampoBatalla.survivor("ab # de [fgh] ijk")).isEqualTo("fgh");
+        assertThat(CampoBatalla.survivor("ab # de [aaaa] ijk")).isEqualTo("aaaa");
+    }
+
+    @Test
+    public void should_return_empty_if_two_bombs_explode_around(){
+        assertThat(CampoBatalla.survivor("a ## bde [fgh] ijk")).isEqualTo("");
+        assertThat(CampoBatalla.survivor("ab # de [fgh] ij # k")).isEqualTo("");
+    }
+
+
+
+    @Test
+    public void should_saved_if_two_bombs_not_explode_around(){
+        assertThat(CampoBatalla.survivor("a ## bde [fgh] ijk [mn] op")).isEqualTo("mn");
+        assertThat(CampoBatalla.survivor("a # bde [fgh] i # jk [mn] op")).isEqualTo("mn");
+    }
+
 }
